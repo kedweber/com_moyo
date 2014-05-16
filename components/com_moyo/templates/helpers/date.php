6343b22a799a7911e1f17ee5eb206fe04b6e3744
Jasper van Rijbroek <jasper199069@live.nl>
2014-05-16 14:10:49 +0200

    Readded check which Joep created.

--- components/com_moyo/templates/helpers/date.php
+++ components/com_moyo/templates/helpers/date.php
@@ -18,6 +18,10 @@ class ComMoyoTemplateHelperDate extends ComDefaultTemplateHelperDate
         // All we have to do is change the LC_TIME locale and the rest of the default behavior is fine.
         setlocale(LC_TIME, str_replace('-', '_', JFactory::getLanguage()->getTag()) . (strpos($_SERVER['HTTP_USER_AGENT'], 'OS X') ? '.UTF-8' : '.utf8'));
 
+        if(!$config['date'] || $config['date'] == '0000-00-00' || $config['date'] == '1970-01-01') {
+            return '';
+        }
+
         return parent::format($config);
     }
 }

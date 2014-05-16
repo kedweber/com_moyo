ca01781519824b77317dec6f2e7fd94199d106cd
Jasper van Rijbroek <jasper199069@live.nl>
2014-05-16 14:14:48 +0200

    Moved array to KConfig.

--- components/com_moyo/templates/helpers/date.php
+++ components/com_moyo/templates/helpers/date.php
@@ -15,10 +15,15 @@ class ComMoyoTemplateHelperDate extends ComDefaultTemplateHelperDate
 {
     public function format($config = array())
     {
+        $config = new KConfig($config);
+        $config->append(array(
+            'date' => '0000-00-00'
+        ));
+
         // All we have to do is change the LC_TIME locale and the rest of the default behavior is fine.
         setlocale(LC_TIME, str_replace('-', '_', JFactory::getLanguage()->getTag()) . (strpos($_SERVER['HTTP_USER_AGENT'], 'OS X') ? '.UTF-8' : '.utf8'));
 
-        if(!$config['date'] || $config['date'] == '0000-00-00' || $config['date'] == '1970-01-01') {
+        if($config->date == '0000-00-00' || $config->date == '1970-01-01') {
             return '';
         }
 

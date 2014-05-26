b1cabb9fcd43b9993174b457b9e090189f2e4668
jvdheijden <joep.van.der.heijden@moyoweb.nl>
2014-05-26 10:48:52 +0200

    Made empty string return condition configurable

--- components/com_moyo/templates/helpers/date.php
+++ components/com_moyo/templates/helpers/date.php
@@ -17,15 +17,16 @@ class ComMoyoTemplateHelperDate extends ComDefaultTemplateHelperDate
     {
         $config = new KConfig($config);
         $config->append(array(
-            'date' => '0000-00-00'
+            'date' => '0000-00-00',
+            'display_none' => array('0000-00-00', '1970-01-01')
         ));
+
+        if (in_array($config->date, $config->display_none->toArray())) {
+            return '';
+        }
 
         // All we have to do is change the LC_TIME locale and the rest of the default behavior is fine.
         setlocale(LC_TIME, str_replace('-', '_', JFactory::getLanguage()->getTag()) . (strpos($_SERVER['HTTP_USER_AGENT'], 'OS X') ? '.UTF-8' : '.utf8'));
-
-        if($config->date == '0000-00-00' || $config->date == '1970-01-01') {
-            return '';
-        }
 
         return parent::format($config);
     }

11f8ed25fa2d0cd0422b6343e4611d474dd4040b
jvdheijden <joep.van.der.heijden@moyoweb.nl>
2014-09-25 13:50:24 +0200

    docs fixed

--- packages/component/components/com_moyo/templates/helpers/parser.php
+++ packages/component/components/com_moyo/templates/helpers/parser.php
@@ -1,12 +1,9 @@
 <?php
 
 /**
- * Com
+ * ComMoyo
  *
  * @author 		Joep van der Heijden <joep.van.der.heijden@moyoweb.nl>
- * @category
- * @package
- * @subpackage
  */
 
 defined('KOOWA') or die('Restricted Access');
